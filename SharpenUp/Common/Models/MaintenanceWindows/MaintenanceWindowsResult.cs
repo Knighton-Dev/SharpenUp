da2856916d3f2fc9a0bf3aabb60eaab6d1062275
Ian Knighton <ian@knighton.dev>
2020-02-26 14:38:43 -0700

    Added GetMaintenanceWindows
    Can't currently test since I don't have a premium account.

---
+++ SharpenUp/Common/Models/MaintenanceWindows/MaintenanceWindowsResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using SharpenUp.Common.Types;
+
+namespace SharpenUp.Common.Models.MaintenanceWindows
+{
+    public class MaintenanceWindowsResult
+    {
+        [JsonProperty( PropertyName = "stat" )]
+        public RequestStatusType Status { get; set; }
+
+        [JsonProperty( PropertyName = "pagination" )]
+        public Pagination Pagination { get; set; }
+
+        [JsonProperty( PropertyName = "error" )]
+        public Error Error { get; set; }
+
+        [JsonProperty( PropertyName = "mwindows" )]
+        public List<MaintenanceWindow> Results { get; set; }
+    }
+}
